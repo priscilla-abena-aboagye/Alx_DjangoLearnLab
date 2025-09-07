dfa850b2489ff29d3c7341d74bcb9fcf757b3a18
Abena <priscillaabenaaboagye@gmail.com>
2025-09-07 20:31:09 +0000

    fix: LibraryProject/relationship_app/views.py doesn't contain: ["from django.views.generic.detail import DetailView"]

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Library
 from .models import Book
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 
