8212731eca5f5aa15e4df811565b4bdefe1cbbd7
Abena <priscillaabenaaboagye@gmail.com>
2025-09-07 20:37:44 +0000

    fix: LibraryProject/relationship_app/urls.py  doesn't contain: ["LibraryDetailView"]

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -10,7 +10,7 @@ def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
 
-class LibraryView(DetailView):
+class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
