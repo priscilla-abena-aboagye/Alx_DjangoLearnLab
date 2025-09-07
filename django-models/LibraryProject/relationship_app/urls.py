72d3525f03170ada1813777c58871d6499f570d4
Abena <priscillaabenaaboagye@gmail.com>
2025-09-07 22:59:15 +0000

    done

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -14,5 +14,10 @@ urlpatterns = [
     path('admin_view/', views.admin_view, name='admin_view'),
     path('librarian_view/', views.librarian_view, name='librarian_view'),
     path('member_view/', views.member_view, name='member_view'),
+
+    path('books/', views.list_books, name='list_books'),
+    path('book/add/', views.add_book, name='add_book'),
+    path('book/edit/<int:pk>/', views.edit_book, name='edit_book'),
+    path('book/delete/<int:pk>/', views.delete_book, name='delete_book'),
 ]
 
