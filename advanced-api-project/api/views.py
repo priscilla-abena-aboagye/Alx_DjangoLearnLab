5e7df3c3f85dd50b3046f9c0ab2a5a9076ce99fd
Abena <priscillaabenaaboagye@gmail.com>
2025-09-28 13:28:41 +0000

    fix: api/views.py doesn't contain: ["from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated"]

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from .models import Book
 from .serializers import BookSerializer
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
