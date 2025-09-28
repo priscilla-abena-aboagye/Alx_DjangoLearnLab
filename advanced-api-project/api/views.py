36411cbd78c2f041ebee274011d41a5c8f161274
Abena <priscillaabenaaboagye@gmail.com>
2025-09-28 13:02:52 +0000

    fix: api/views.py doesn't contain: ["ListView", "UpdateView", "DeleteView"]

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,18 +2,19 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-class BookListCreateView(generics.ListCreateAPIView):
+class BookListCreateAPI(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
     # Only users that are authenticated can create, but anyone can list
     def get_permissions(self):
-        if self.request.method == "Post":
+        if self.request.method == "POST":  # Fix typo: must be uppercase POST
             return [permissions.IsAuthenticated()]
         return [permissions.AllowAny()]
     
-    # filtering by query parameters
+    # Filtering by query parameters
     def get_queryset(self):
         queryset = Book.objects.all()
         author_id = self.request.query_params.get('author')
@@ -21,7 +22,7 @@ class BookListCreateView(generics.ListCreateAPIView):
             queryset = queryset.filter(author__id=author_id)
         return queryset
     
-class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+class BookDetailAPI(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
@@ -31,3 +32,26 @@ class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
             return [permissions.IsAuthenticated()]
         return [permissions.AllowAny()]
 
+
+class BookListView(ListView):
+    model = Book
+    template_name = "book_list.html"
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = "book_detail.html"
+
+class BookCreateView(CreateView):
+    model = Book
+    fields = ["title", "publication_year", "author"]
+    template_name = "book_form.html"
+
+class BookUpdateView(UpdateView):
+    model = Book
+    fields = ["title", "publication_year", "author"]
+    template_name = "book_form.html"
+
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = "book_confirm_delete.html"
+    success_url = "/books/"
