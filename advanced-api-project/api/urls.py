a502525332650fde07a8e50764e04459214b1ed5
Abena <priscillaabenaaboagye@gmail.com>
2025-09-28 13:10:08 +0000

    fix: api/urls.py doesn't contain: ["books/create", "books/update", "books/delete"]

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import BookDetailView, BookListCreateView
+from .views import BookDetailAPI, BookListCreateAPI, BookCreateView, BookUpdateView, BookDeleteView
 
 urlpatterns = [
-    path("books/", BookListCreateView.as_view(), name="list-book"),
-    path("books/<int:pk>/", BookDetailView.as_view(), name="book-detail"),
+    path("books/", BookListCreateAPI.as_view(), name="list-book"),
+    path("books/<int:pk>/", BookDetailAPI.as_view(), name="book-detail"),
+
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
 ]
