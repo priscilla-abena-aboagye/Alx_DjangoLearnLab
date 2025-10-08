a4a25737343e75565225a4376dde086ca64ab035
Abena <priscillaabenaaboagye@gmail.com>
2025-10-08 17:25:20 +0000

    fix:Checks for accounts/serializers.py doesn't contain: ["serializers.CharField()"]

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -11,7 +11,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
 
     class Meta:
         model = User
